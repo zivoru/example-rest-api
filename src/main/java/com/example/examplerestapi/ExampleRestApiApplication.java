43e58402dcac7107803eec631fc77ea5f0d100f0
zivo <swift2002999@gmail.com>
2023-01-15 18:04:17 +0500

    Initial commit

---
+++ src/main/java/com/example/examplerestapi/ExampleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examplerestapi/ExampleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
